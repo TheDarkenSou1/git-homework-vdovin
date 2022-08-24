b55673c4d4107244bf7f1509b74fd3306e07559c
Andrey Vdovin <99421225+TheDarkenSou1@users.noreply.github.com>
2022-08-24 16:16:32 +0300

    Add files via upload

---
+++ Дз №4 Вдовін.py
@@ -0,0 +1,48 @@
+import math
+print("Завдання 1")
+num = input("Введіть номер квитка: ")
+while True:
+    N = list(num)
+    for index in range(len(N)):
+        N[index] = int(N[index])
+    if len(N) % 2:
+        print("Номер квитка має містити однакову\n"
+              "кількість цифр з кожної сторони!\n")
+        num = input("Введіть номер квитка ще раз: ")
+    else:
+        i = int(len(N) / 2)
+        N1 = N[0:i]
+        N2 = N[-i:]
+        if sum(N1) == sum(N2):
+            print("Ваш квиток щасливий!\n")
+        else:
+            print("Ваш квиток нещасливий. Пощастить наступного разу!\n")
+        break
+print("Завдання 2")
+number = input("Введіть число: ")
+M = list(number)
+for index in range(len(M)):
+    M[index] = int(M[index])
+a = 0
+b = -1
+is_palindrome = True
+for i in M:
+    if M[a] == M[b]:
+        a += 1
+        b -= 1
+        continue
+    else:
+        is_palindrome = False
+if is_palindrome == True:
+    print("Ваше число - паліндром.\n")
+else:
+    print("Ваше число - не паліндромом.\n")
+print("Завдання 3")
+X = int(input("Введіть координати X: "))
+Y = int(input("Введіть координати Y: "))
+r = 4
+r_XY = math.sqrt(X ** 2 + Y ** 2)
+if r_XY <= r:
+    print("Точка належить колу.")
+else:
+    print("Точка не належить колу.")
