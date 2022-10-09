2ba72315f641e39080322f5c3871127ba4ed5fa9
Andrey Vdovin <andrejvdovin973@gmail.com>
2022-10-09 21:41:34 +0300

    Дз №15 дороблено.

--- Дз №15 Вдовін.py
+++ Дз №15 Вдовін.py
@@ -1,130 +1,130 @@
-# print("Завдання 1\n")
-#
-# class LimitGroupError(Exception):
-#
-#     def __init__(self, value=None, msg=None):
-#         super().__init__()
-#         self.value = value
-#         self.msg = msg
-#
-#     def __str__(self):
-#         return f'Current Limit in Group = {self.value}\n{self.msg}'
-#
-# class Person:
-#     """
-#     surname and name of person
-#     """
-#     def __init__(self, surname, name):
-#         self.surname = surname
-#         self.name = name
-#
-#     def __str__(self):
-#         return f"{self.surname} {self.name}"
-#
-# class Student(Person):
-#     """
-#     info student
-#     """
-#     def __init__(self, surname : str, name : str, birthday_data):
-#         super().__init__(surname, name)
-#         self.birthday_data = birthday_data
-#
-#     def __str__(self):
-#         return f"{self.surname} {self.name}, {self.birthday_data}"
-#
-# class GroupIterator:
-#
-#     def __init__(self, wrapped):
-#         self.wrapped = wrapped
-#         self.index = 0
-#
-#     def __next__(self):
-#         if self.index < len(self.wrapped):
-#             self.index += 1
-#             return self.wrapped[self.index - 1]
-#         else:
-#             raise StopIteration
-#
-#     def __iter__(self):
-#         return self
-#
-# class Group:
-#     """
-#     creation the group of students
-#     """
-#     count = 0
-#     def __init__(self, title, max_students):
-#         self.max_students = max_students
-#         self.title = title
-#         self.students = []
-#
-#     def add_students(self, student: Student):
-#         if not isinstance(student, Student):
-#             raise TypeError('Smth wrong with data type')
-#
-#         if len(self.students) >= self.max_students:
-#             raise LimitGroupError(self.max_students, "Max limit error")
-#
-#         if student in self.students:
-#             raise ValueError('Student already registered')
-#
-#         self.students.append(student)
-#
-#     def search_students(self, surname : str):
-#         res = []
-#         for student in self.students:
-#             if surname.strip().lower() == student.surname.lower():
-#                 res.append(student)
-#         return res
-#
-#     def remove_students(self, student):
-#         if isinstance(student, Student):
-#             if student in self.students:
-#                 self.students.remove(student)
-#         else:
-#             raise TypeError('Smth wrong with data type')
-#
-#     def __iter__(self):
-#         return GroupIterator(self.students)
-#
-#     def __len__(self):
-#         return len(self.students)
-#
-#     def __str__(self):
-#         return f"{self.title}\n" + "\n".join(map(str, self.students))
-#
-# st1 = Student("Вдовін", "Андрій", "26.07.2003")
-# st2 = Student("Пиріжок", "Максим", "01.03.2001")
-# st3 = Student("Арбуз", "Віктор", "14.04.2004")
-# st4 = Student("Кавун", "Роман", "16.05.2002")
-# st5 = Student("Персик", "Олександр", "10.06.2003")
-# st6 = Student("Слива", "Григорій", "09.08.2002")
-# st7 = Student("Абрикос", "Марія", "22.09.2002")
-# st8 = Student("Вишня", "Дарья", "23.10.2001")
-# st9 = Student("Полуниця", "Люба", "29.11.2001")
-# st10 = Student("Батьківна", "Олена", "27.12.2000")
-#
-# gr1 = Group("Python PRO", max_students=10)
-#
-# try:
-#     gr1.add_students(st1)
-#     gr1.add_students(st2)
-#     gr1.add_students(st3)
-#     gr1.add_students(st4)
-#     gr1.add_students(st5)
-#     gr1.add_students(st6)
-#     gr1.add_students(st7)
-#     gr1.add_students(st8)
-#     gr1.add_students(st9)
-#     gr1.add_students(st10)
-# except Exception as error:
-#     print(error)
-#
-#
-# count = len(gr1.students)
-#
-# for person in range(count):
-#     print(gr1.students[person], f": {person + 1} у групі.")
+print("Завдання 1\n")
+
+class LimitGroupError(Exception):
+
+    def __init__(self, value=None, msg=None):
+        super().__init__()
+        self.value = value
+        self.msg = msg
+
+    def __str__(self):
+        return f'Current Limit in Group = {self.value}\n{self.msg}'
+
+class Person:
+    """
+    surname and name of person
+    """
+    def __init__(self, surname, name):
+        self.surname = surname
+        self.name = name
+
+    def __str__(self):
+        return f"{self.surname} {self.name}"
+
+class Student(Person):
+    """
+    info student
+    """
+    def __init__(self, surname : str, name : str, birthday_data):
+        super().__init__(surname, name)
+        self.birthday_data = birthday_data
+
+    def __str__(self):
+        return f"{self.surname} {self.name}, {self.birthday_data}"
+
+class GroupIterator:
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.wrapped):
+            self.index += 1
+            return self.wrapped[self.index - 1]
+        else:
+            raise StopIteration
+
+    def __iter__(self):
+        return self
+
+class Group:
+    """
+    creation the group of students
+    """
+    count = 0
+    def __init__(self, title, max_students):
+        self.max_students = max_students
+        self.title = title
+        self.students = []
+
+    def add_students(self, student: Student):
+        if not isinstance(student, Student):
+            raise TypeError('Smth wrong with data type')
+
+        if len(self.students) >= self.max_students:
+            raise LimitGroupError(self.max_students, "Max limit error")
+
+        if student in self.students:
+            raise ValueError('Student already registered')
+
+        self.students.append(student)
+
+    def search_students(self, surname : str):
+        res = []
+        for student in self.students:
+            if surname.strip().lower() == student.surname.lower():
+                res.append(student)
+        return res
+
+    def remove_students(self, student):
+        if isinstance(student, Student):
+            if student in self.students:
+                self.students.remove(student)
+        else:
+            raise TypeError('Smth wrong with data type')
+
+    def __iter__(self):
+        return GroupIterator(self.students)
+
+    def __len__(self):
+        return len(self.students)
+
+    def __str__(self):
+        return f"{self.title}\n" + "\n".join(map(str, self.students))
+
+st1 = Student("Вдовін", "Андрій", "26.07.2003")
+st2 = Student("Пиріжок", "Максим", "01.03.2001")
+st3 = Student("Арбуз", "Віктор", "14.04.2004")
+st4 = Student("Кавун", "Роман", "16.05.2002")
+st5 = Student("Персик", "Олександр", "10.06.2003")
+st6 = Student("Слива", "Григорій", "09.08.2002")
+st7 = Student("Абрикос", "Марія", "22.09.2002")
+st8 = Student("Вишня", "Дарья", "23.10.2001")
+st9 = Student("Полуниця", "Люба", "29.11.2001")
+st10 = Student("Батьківна", "Олена", "27.12.2000")
+
+gr1 = Group("Python PRO", max_students=10)
+
+try:
+    gr1.add_students(st1)
+    gr1.add_students(st2)
+    gr1.add_students(st3)
+    gr1.add_students(st4)
+    gr1.add_students(st5)
+    gr1.add_students(st6)
+    gr1.add_students(st7)
+    gr1.add_students(st8)
+    gr1.add_students(st9)
+    gr1.add_students(st10)
+except Exception as error:
+    print(error)
+
+
+count = len(gr1.students)
+
+for person in range(count):
+    print(gr1.students[person], f": {person + 1} у групі.")
 
 print("\nЗавдання 2\n")
 
